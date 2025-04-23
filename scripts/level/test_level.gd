db2f8d8dd2923681083a497c9e12626c73496a3b
Carbon <calumgcoppack@gmail.com>
2025-04-23 03:12:25 -0400

    Fixed test level

--- scripts/level/test_level.gd
+++ scripts/level/test_level.gd
@@ -1,17 +1,8 @@
 extends Level
 
 func _play() -> void:
-	var enemy = enemy_template.instantiate()
 	var posx = randf_range(100, 360)
+	var enemy_tick = func (age: float, _position: Vector2, _level: Level):
+		_level.spawn_ring(_position, BulletType.SMALL_BALL, 5, age * PI/15, 5, 40, 20)
 	
-	enemy.position = Vector2(posx, -60)
-	enemy.destination = Vector2(posx, randf_range(130, 200))
-	enemy.target_dest = true
-	enemy.tick_duration = randf_range(0.1, 0.4)
-	
-	var enemy_shots = randi_range(3, 6)
-	
-	enemy.tick_func = func (age: float, _position: Vector2):
-		GameController.spawn_ring(_position, "small_ball", enemy_shots, age * PI/15, 5, 40, 20)
-	
-	add_child(enemy)
+	spawn_enemy(Vector2(posx, -60), Vector2(posx, randf_range(130, 200)), randf_range(0.1, 0.4), enemy_tick)

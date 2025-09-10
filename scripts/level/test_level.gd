5faa5cc830afe410d622f42e9c86096cb2571a73
Carbon <calumgcoppack@gmail.com>
2025-09-10 00:31:22 -0400

    Separate enemy hitbox and hurtbox

--- scripts/level/test_level.gd
+++ scripts/level/test_level.gd
@@ -5,13 +5,13 @@ var end_dialogue_flag = false
 
 func _build_level() -> void:
 	sleep(2)
-	spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
-		lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
-	spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
-		lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
-	spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
-		lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
-	sleep(10)
+	#spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
+		#lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
+	#spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
+		#lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
+	#spawn_enemy_wave(10, 1, Vector2(100,0), Vector2(100, 2000), 1, func(l: float, p: Vector2, lvl: Level): 
+		#lvl.bullet_burst(p, BulletType.KNIFE_GOLD, 8, PI/4, l*PI/32, 5, 100, 45))
+	#sleep(10)
 	
 	spawn_boss("crystal_boss", BOSS_DEFAULT_POSITION, BOSS_OFFSCREEN_POSITION)
 	sleep(3)
